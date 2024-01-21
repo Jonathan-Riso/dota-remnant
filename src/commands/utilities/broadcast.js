c496645d1c1cd1308164018db18c98d47a94dee1
Jonathan <riso.jona10@gmail.com>
2024-01-20 19:41:13 -0500

    Fixed end of game spam

--- src/commands/utilities/broadcast.js
+++ src/commands/utilities/broadcast.js
@@ -4,6 +4,8 @@ const { GSI } = require('../../index')
 const { gamestates } = require('../../helpers/gamestate')
 const { inProgressEmbed } = require('../../helpers/embedHelper')
 
+var gameEnd = false;
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('broadcast')
@@ -19,29 +21,34 @@ module.exports = {
 					console.log("Not in game")
 
 				} else if (dota2.map && dota2.map.game_state == gamestates["draft"]){
+					gameEnd=false;
 					console.log("In Draft")
 					await msg.edit("In Draft");
 					
 					
 				} else if (dota2.map && dota2.map.game_state == gamestates["maploading"]){
+					gameEnd=false;
 					console.log("Map is loading")
 					await msg.edit("Map is Loading");
 					
 				} else if (dota2.map && dota2.map.game_state == gamestates["pregame"]){
+					gameEnd=false;
 					console.log("In Pregame")
 					const remnantEmbed = inProgressEmbed(dota2)
 					await msg.edit({ embeds: [remnantEmbed]});
 
 					
 				} else if (dota2.map && dota2.map.game_state == gamestates["inprogress"]){
+					gameEnd=false;
 					console.log("In Progress")
 					const remnantEmbed = inProgressEmbed(dota2)
 					await msg.edit({ embeds: [remnantEmbed]});
 
-				} else if (dota2.map && dota2.map.game_state == gamestates["postgame"]){
+				} else if (dota2.map && dota2.map.game_state == gamestates["postgame"] && !gameEnd){
 					console.log("Post game")
 					await interaction.channel.send(`Game is over, ${dota2.map.win_team} Victory`);
 					msg = await interaction.channel.send("Waiting for player to join game.");
+					gameEnd = true;
 				}
 		});
 	},
