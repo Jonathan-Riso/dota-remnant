af62138c8065da75bf3d7bf36224bb4cb07bd216
Jonathan Riso <riso.jona10@gmail.com>
2024-01-23 10:47:40 -0500

    Removed some bloated logging

--- src/commands/utilities/broadcast.js
+++ src/commands/utilities/broadcast.js
@@ -10,34 +10,29 @@ var gameEnd = false;
 async function beginBroadcast(dota2){
 	try{
 		if (!dota2.map) {
-			console.debug("Not in game");
+			console.warn("Not in game");
 
 		} else if (dota2.map && dota2.map.game_state == gamestates["draft"]){
 			gameEnd=false;
-			console.debug("In Draft");
 			await msg.edit("In Draft");
 			
 			
 		} else if (dota2.map && dota2.map.game_state == gamestates["maploading"]){
 			gameEnd=false;
-			console.debug("Map is loading");
 			await msg.edit("Map is Loading");
 			
 		} else if (dota2.map && dota2.map.game_state == gamestates["pregame"]){
 			gameEnd=false;
-			console.debug("In Pregame");
 			const remnantEmbed = inProgressEmbed(dota2)
 			await msg.edit({ content: "Current Game", embeds: [remnantEmbed]});
 
 			
 		} else if (dota2.map && dota2.map.game_state == gamestates["inprogress"]){
 			gameEnd=false;
-			console.debug("In Progress");
 			const remnantEmbed = inProgressEmbed(dota2)
 			await msg.edit({ content: "Current Game", embeds: [remnantEmbed]});
 
 		} else if (dota2.map && dota2.map.game_state == gamestates["postgame"] && !gameEnd){
-			console.debug("Post game");
 			await channel.send(`Game is over, ${dota2.map.win_team} victory`);
 			msg = await channel.send("Waiting for player to join game.");
 			gameEnd = true;
