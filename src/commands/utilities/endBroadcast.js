985428b2d87142ce02431db1464ae0d8efcd4ed6
Jonathan Riso <riso.jona10@gmail.com>
2024-01-22 21:40:14 -0500

    Removed some logging.

--- src/commands/utilities/endBroadcast.js
+++ src/commands/utilities/endBroadcast.js
@@ -8,14 +8,12 @@ module.exports = {
 		.setDescription('Ends the current broadcast if it exists.'),
 	async execute(interaction) {
         try{
-            console.log(GSI.listenerCount('data'))
             if(GSI.listenerCount('data') > 0) {
                 GSI.removeAllListeners('data'); // Temporary until I get removeListener working.
                 await interaction.reply('Broadcast Ended');
             } else {
                 await interaction.reply({ content: 'There are no active broadcasts, use the broadcast command to start one.', ephemeral: true });
             }
-            console.log(GSI.listenerCount('data'))
         } catch (err){
             console.error(err);
         }
